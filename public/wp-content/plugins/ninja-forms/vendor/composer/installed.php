ca097d53459a752c6ae64da406fec4b48d5bbffa
shahzaib360 <shahzaib.ajmal@hotmail.com>
2025-10-06 03:41:35 +0500

    Sangees WordPress

---
+++ public/wp-content/plugins/ninja-forms/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'saturday-drive/ninja-forms',
+        'pretty_version' => 'dev-75e44baebba4533a9d2695eaf4e1566f42b2b4a6',
+        'version' => 'dev-75e44baebba4533a9d2695eaf4e1566f42b2b4a6',
+        'reference' => '75e44baebba4533a9d2695eaf4e1566f42b2b4a6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'saturday-drive/ninja-forms' => array(
+            'pretty_version' => 'dev-75e44baebba4533a9d2695eaf4e1566f42b2b4a6',
+            'version' => 'dev-75e44baebba4533a9d2695eaf4e1566f42b2b4a6',
+            'reference' => '75e44baebba4533a9d2695eaf4e1566f42b2b4a6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
